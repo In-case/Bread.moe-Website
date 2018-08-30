5e8fd7aa968a8bf2c2cc261f75b1b045ab99b40a
Unknown <jake@gealer.email>
2018-08-30 12:59:46 +0100

    More stuff

---
+++ template-generation.js
@@ -0,0 +1,19 @@
+function getURLValue(key) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
+// Gets the value from the URL
+
+const keyFromURL = getURLValue("key");
+if (keyFromURL) { $("#keyField").val(keyFromURL); }
+// Gets the key from the URL.
