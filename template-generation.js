f9c03f6333bce1304c8f07de85db6695a5457316
Unknown <jake@gealer.email>
2018-08-30 18:48:09 +0100

    More stuff

--- template-generation.js
+++ template-generation.js
@@ -20,3 +20,39 @@ $(document).ready(async () => {
     // Gets the key from the URL.
 });
 // Handles the window loading.
+
+async function showError(error) {
+    await $("#badThingsHappened").val(error);
+    if (await $("#badThingsHappened").hasClass("is-hidden")) {
+        await $("#badThingsHappened").removeClass("is-hidden");
+    }
+}
+// Pops up with a error.
+
+async function handleDomainKey() {
+    const key = await $("#keyField").val().replace(/ /g,'');
+    const domain = await $("#domainField").val().toLowerCase().replace(/ /g,'');
+    if (key === "") {
+        await showError("No key was provided.");
+    } else {
+        if (domain === "") {
+            await showError("No domain was provided.");
+        } else {
+            if (!/^[a-z0-9-]{1,63}\.(bread\.moe|ate-a-ta\.co|blobs\.party|died-at-a\.party|died-while\.racing|hacked-my\.stream|hacked-my\.webcam|had-a\.party|is-a-ta\.co|pinged-b1nzy-for\.fun|stop-pings\.me|tits-are\.life|uses-ifunny\.co|wears-supre\.me|went-to-the-gucci\.store|worstme\.me|tried-to-do\.science)$/.test(domain)) {
+                await showError("The domain given is invalid.")
+            } else {
+                return key, domain;
+            }
+        }
+    }
+}
+// Handles getting the domain and key.
+
+async function downloadShareX() {
+    const result = await handleDomainKey();
+    if (result === undefined) {
+        return;
+    }
+    const key, domain = result;
+}
+// Creates the ShareX template.
